7a47d4a6a1ef27d30c266e68a50e3ee282af3f1d
vincenzo8825 <vincenzorocca88@gmail.com>
2023-10-11 16:19:37 +0200

    Esercizio S3G3

---
+++ m1/s3/g3/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let taskInput = document.getElementById('taskInput');
+    let addTaskButton = document.getElementById('aggiungi task');
+    let taskList = document.getElementById('taskList');
+    let clockDisplay = document.getElementById('orologio');
+
+    addTaskButton.addEventListener('click', function () {
+        let taskText = taskInput.value.trim();
+
+        if (taskText !== '') {
+            let li = document.createElement('li');
+            li.textContent = taskText;
+
+            li.addEventListener('click', function () {
+                if (li.style.textDecoration === '' || li.style.textDecoration === 'none') {
+                    li.style.textDecoration = 'line-through';
+                } else {
+                    li.style.textDecoration = 'none';
+                }
+            });
+
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = 'elimina';
+
+            deleteButton.addEventListener('click', function () {
+                taskList.removeChild(li);
+            });
+
+            li.appendChild(deleteButton);
+            taskList.appendChild(li);
+            taskInput.value = '';
+        }
+    });
+
+    function updateClock() {
+        let now = new Date();
+        let hours = now.getHours().toString().padStart(2, '0');
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        let seconds = now.getSeconds().toString().padStart(2, '0');
+        clockDisplay.textContent = hours + ':' + minutes + ':' + seconds;
+    }
+
+    setInterval(updateClock, 1000);
+
+    updateClock();
+});
+

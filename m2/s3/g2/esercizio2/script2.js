8cb64812acb31d80340d39e7cc079fc745b4bd82
vincenzo8825 <vincenzorocca88@gmail.com>
2023-11-07 15:32:41 +0100

    Esercizio-M2-S3-G2

--- m2/s3/g2/esercizio2/script2.js
+++ m2/s3/g2/esercizio2/script2.js
@@ -1,5 +1,5 @@
 
-
+//Crea un contatore che tenga conto del tempo che passa, utilizzando sessionStorage. Aggiornando la pagina il valore prosegue, chiudendo la pagina - ovviamente - ricomincia. Il valore del contatore deve aggiornarsi ad ogni secondo
 
 
 

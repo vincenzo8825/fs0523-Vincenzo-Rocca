cd333eb54500e70b43b0b5de947f44c10ea58398
vincenzo8825 <vincenzorocca88@gmail.com>
2023-11-27 16:36:42 +0100

    Esercizio M3-S2-G1

---
+++ m3/s2/g1/esercizio/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    Test2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

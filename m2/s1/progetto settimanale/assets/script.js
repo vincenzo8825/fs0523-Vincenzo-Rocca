b51bed8ad0b0a4624f31f5e52f4f73ad74ccd731
vincenzo8825 <vincenzorocca88@gmail.com>
2024-02-21 16:06:41 +0100

    fix header

--- m2/s1/progetto settimanale/assets/script.js
+++ m2/s1/progetto settimanale/assets/script.js
@@ -1,13 +1,35 @@
-
-let topBar = document.querySelector(".topBar");
-let button = document.querySelector(".topBar .blackButton");
-
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 550) {
-        topBar.classList.add("newTopBar");
-        button.classList.add("newButton");
-    } else {
-        topBar.classList.remove("newTopBar");
-        button.classList.remove("newButton");
+class PageAnimator {
+    constructor() {
+        this.topBar = document.querySelector(".topBar");
+        this.button = document.querySelector(".topBar .blackButton");
+        this.letters = document.querySelectorAll('.animation svg g');
+        this.initEvents();
     }
-});
+
+    initEvents() {
+        window.addEventListener("scroll", () => this.handleScroll());
+        setInterval(() => this.animateLetters(), 2);
+    }
+
+    handleScroll() {
+        if (window.scrollY > 550) {
+            this.topBar.classList.add("newTopBar");
+            this.button.classList.add("newButton");
+        } else {
+            this.topBar.classList.remove("newTopBar");
+            this.button.classList.remove("newButton");
+        }
+    }
+
+    animateLetters() {
+        let randomElement = this.letters[Math.floor(Math.random() * this.letters.length)];
+        if (randomElement.classList.contains('letterM')) {
+            randomElement.classList.remove('letterM');
+        } else {
+            randomElement.classList.add('letterM');
+        }
+    }
+}
+
+// Crea l'istanza della classe PageAnimator per avviare le animazioni e gli event listener
+const pageAnimator = new PageAnimator();
